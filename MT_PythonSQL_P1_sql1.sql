36686067db38be595acbdc1d2448b05007434273
mtt195 <107153167+mtt195@users.noreply.github.com>
2023-12-06 00:00:07 +0100

    2. SQL code to create sample data

---
+++ MT_PythonSQL_P1_sql1.sql
@@ -0,0 +1,17 @@
+/* -------------------------------------------------- */
+/* AUTHOR NAME: MACIEJ TOMASZEWSKI                    */
+/* CREATE DATE: 02.12.2023                            */
+/* DESCRIPTION:                                       */
+/*  MY FIRST PYTHON & SQL PROJECT. THIS SCRIPT IS     */
+/*  USED TO CREATE A DATABASE FOR THE PURPOSE OF      */
+/*  THE PROJECT. THIS IS THE FIRST OF 3 SQL SCRIPTS.  */
+
+
+/*                  SQL SCRIPT 1/3                    */
+/* -------------------------------------------------- */
+
+/*
+DROP DATABASE MT_PythonSQL_Project1;
+*/
+
+CREATE DATABASE MT_PythonSQL_Project1;
